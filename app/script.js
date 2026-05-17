fdae20ae6fa83e8bbd1ee3717685898051a0b072
refarahmawati555699-pixel <refarahmawati555699@mail.ugm.ac.id>
2026-05-17 16:58:12 +0700

    add responsive hamburger navbar

--- app/script.js
+++ app/script.js
@@ -46,3 +46,10 @@ async function getRooms() {
 }
 
 getRooms();
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
